8d4590b21b816b65762750c91f595c5daceded36
starfishlover62 <122428974+starfishlover62@users.noreply.github.com>
2025-04-03 23:44:21 -0700

    Create example4.c

---
+++ example4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Goal is to have dangling pointer example 
+  May also have example about using an uninitialized pointer */
+
+// Fills the array with random integers from 0-9
+int* generateRandomData(int num_elements){
+  if(i <= 0){
+    return NULL;
+  }
+  int array[num_elements];
+  for(int i = 0; i < num_elements; ++i){
+    array[i] = random % 10; // Fills array with a random number 0-9
+  }
+  return array;
+}
+
+// Prints out an array of integers
+void printArray(int* array, int num_elements){
+  for(int i = 0; i < num_elements; ++i){
+    printf("%d",array[i]);
+    if(i != 0 && (i%5 == 0)){
+      printf("\n");
+    } else {
+      printf(" ");
+    }
+  }
+}
+
+
+int main() {
+  srandom(ctime(NULL));
+  int num_elements = 10;
+  int* data = generateRandomData(num_elements);
+  printArray(data,num_elements);
+  return 0;
+}
