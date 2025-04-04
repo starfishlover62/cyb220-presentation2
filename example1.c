6afe5e4796d10c8d04777512576c8e744dff4231
starfishlover62 <122428974+starfishlover62@users.noreply.github.com>
2025-04-03 23:35:54 -0700

    Update example1.c

--- example1.c
+++ example1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-	int* data = malloc(sizeof(int)*10);
+	int* data = malloc(sizeof(int)*10); // No checks for malloc being successful
   free(data);
 return 0;
 }
