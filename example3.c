c77b2bc830da2cb53412ea198ece5e92823ba9d9
starfishlover62 <122428974+starfishlover62@users.noreply.github.com>
2025-04-03 23:35:17 -0700

    Create example3.c

---
+++ example3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+  Needs to be improved. String search example might be nice. It should be fairly complex
+  Probably should be within its own function, called from main.
+*/
+
+int main() {
+	int i = 0;
+	int array[10];
+	for(;i < 10; ++i){
+		array[i] = i; 
+}
+printf(“final value: %d\n”,array[i]); // i=10 at this point, causing OOB array access.
+return 0;
+}
