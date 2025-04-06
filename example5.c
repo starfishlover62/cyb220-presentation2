fd50de66ee7990586364beba677cf883c8748a2c
JMGillum <JMGillum@protonmail.com>
2025-04-05 19:51:23 -0700

    Updated to be in function

--- example5.c
+++ example5.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main() {
+void loop(int array[10]) {
+    int i = 0;
     while(1){
-        printf("hello\n");
+        printf("%d\n",array[i]);
+        ++i;
     }
+}
+
+
+int main() {
+    int array[] = {1,2,3,4,5,6,7,8,9,0};
+    loop(array);
     return 0;
 }
